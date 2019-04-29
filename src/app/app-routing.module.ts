ea61711f8cb227c50a3cf7d262faf63b05c35b21
Yanchen <fengyc2018@gmail.com>
2019-04-29 11:21:08 -0400

    merge two routing module files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductCompareComponent } from './product/product-compare/product-compare.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', component: ProductComponent, children: [
+      { path: ':id', component: ProductDetailComponent },
+      { path: 'compare', component:  ProductCompareComponent }
+  ]},
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
